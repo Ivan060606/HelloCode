71a1026aa9b8519cebcae98aea54681c2066dc94
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-08-21 17:21:54 +0300

    example005

---
+++ Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Как тебя зовут?");
+string username = Console.ReadLine();
+if(username.ToLower() == "ivan")
+{Console.Write("Ура, это же Иван!!!");}
+else{Console.Write("Привет, ");
+Console.Write(username);}
